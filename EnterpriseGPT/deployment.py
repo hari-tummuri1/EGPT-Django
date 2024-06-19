f81db4742cdb80e7fad112fecaf4fe12b1d951a9
hari-tummuri <tummurihari@gmail.com>
2024-06-19 13:25:05 +0530

    added secret

--- EnterpriseGPT/deployment.py
+++ EnterpriseGPT/deployment.py
@@ -2,6 +2,7 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
+SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME'], 'http://'+os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
@@ -22,13 +23,18 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'Whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+name = os.environ['AZURE_MYSQL_NAME']
+user = os.environ['AZURE_MYSQL_USER']
+password = os.environ['AZURE_MYSQL_PASSWORD']
+host = os.environ['AZURE_MYSQL_HOST']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'enterprisegpt',  
-        'USER': 'egptadmin',  
-        'PASSWORD': 'Admin@1234',  
-        'HOST': 'egpt.mysql.database.azure.com',  
+        'NAME': name,  
+        'USER': user,  
+        'PASSWORD': password,  
+        'HOST': host,  
         'PORT': '3306'
     }
 }
